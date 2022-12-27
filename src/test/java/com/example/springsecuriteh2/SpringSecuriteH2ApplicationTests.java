5741667ba4d120e035e04f5325541774fec3cc47
madymadesc <madymadesc@gmail.com>
2022-12-27 13:09:01 +0000

    creation du projet

---
+++ src/test/java/com/example/springsecuriteh2/SpringSecuriteH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuriteh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuriteH2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
