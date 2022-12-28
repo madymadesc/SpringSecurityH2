0cd3fd2e115524ac62a867622ceb2a9efe6d8f9f
madymadesc <madymadesc@gmail.com>
2022-12-28 16:22:29 +0000

    commit du security

--- src/main/java/com/example/springsecuriteh2/security/config/SecurityConfig.java
+++ src/main/java/com/example/springsecuriteh2/security/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.springsecuriteh2.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,13 +14,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
+        return auth.getAuthenticationManager();
+    }
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception{
          http
                 .csrf().disable().cors().and()
                  .headers().frameOptions().disable().and()
-                 .authorizeHttpRequests().anyRequest().permitAll();
+                 .formLogin().and()
+                 .authorizeHttpRequests().anyRequest().authenticated();
         return http.build();
     }
 }
