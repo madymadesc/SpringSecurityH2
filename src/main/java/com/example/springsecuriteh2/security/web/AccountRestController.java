8f25f89cf07aa2d9d8ef6fe285fd21be6e6a13b7
madymadesc <madymadesc@gmail.com>
2022-12-28 15:08:27 +0000

    rest controleer

--- src/main/java/com/example/springsecuriteh2/security/web/AccountRestController.java
+++ src/main/java/com/example/springsecuriteh2/security/web/AccountRestController.java
@@ -1,8 +1,12 @@
 package com.example.springsecuriteh2.security.web;
 
+import com.example.springsecuriteh2.security.entity.AppRole;
 import com.example.springsecuriteh2.security.entity.AppUser;
 import com.example.springsecuriteh2.security.service.AccountService;
+import lombok.Data;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,4 +24,25 @@ public class AccountRestController {
     public List<AppUser> appUserList(){
         return accountService.listUsers();
     }
+
+    @PostMapping("/saveUser")
+    public AppUser saveUser(@RequestBody AppUser appUser){
+        return accountService.addNewUser(appUser);
+    }
+
+    @PostMapping("/saveRole")
+    public AppRole saveRole(@RequestBody AppRole appRole){
+        return accountService.addNewRole(appRole);
+    }
+
+    @PostMapping("/addRoleToUser")
+    public void addRoleToUser(@RequestBody RoleUserForm roleUserForm){
+        accountService.addRoleToUser(roleUserForm.getUsername(),roleUserForm.getRoleName());
+    }
+}
+
+@Data
+class RoleUserForm{
+    private String username;
+    private String roleName;
 }
